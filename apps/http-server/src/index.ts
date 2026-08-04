0b59ae4fd15508b0be0b3883e048a1bba5c1d1f0
Shivansh-Pandey-4 <shivanshofficial8750@gmail.com>
2026-08-04 10:19:54 +0530

    feat: add email, password based authentication using better-auth lib

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,24 @@
+import express, { type Request, type Response } from "express";
+import { toNodeHandler, auth } from "@repo/auth";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.HTTP_PORT || 4000;
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response)=>{
+    return res.json({
+        success : true,
+        msg : "hello world"
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`app started listening on the port: ${PORT}`);
+})
