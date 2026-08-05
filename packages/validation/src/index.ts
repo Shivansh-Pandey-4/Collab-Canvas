8ad9aed363275f92e55a14815d08e96678cdbe8c
Shivansh-Pandey-4 <shivanshofficial8750@gmail.com>
2026-08-05 16:03:12 +0530

    feat: add zod validation package

---
+++ packages/validation/src/index.ts
@@ -0,0 +1,5 @@
+import zod from "zod";
+import { createRoomSlugSchema } from "./roomSchema.js";
+
+export {createRoomSlugSchema};
+export default zod;
---
+++ packages/validation/src/roomSchema.ts
@@ -0,0 +1,5 @@
+import zod from "zod";
+
+export const createRoomSlugSchema = zod.object({
+    slug : zod.string().trim().min(3, "minimum 3 characters required").max(100, "maximum 100 characters are allowed")
+});
