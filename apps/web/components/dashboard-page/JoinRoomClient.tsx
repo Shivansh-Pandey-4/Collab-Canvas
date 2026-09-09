1f0119165e8857b2d153eac4fb2670f927013a97
Shivansh-Pandey-4 <shivanshofficial8750@gmail.com>
2026-09-09 12:39:04 +0530

    feat: add create room functionality

--- apps/web/components/dashboard-page/JoinRoomClient.tsx
+++ apps/web/components/dashboard-page/JoinRoomClient.tsx
@@ -3,27 +3,108 @@
 import Button from "@repo/ui/button";
 import Input from "@repo/ui/input";
 import Model from "@repo/ui/model";
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "sonner";
+
+
+interface IData {
+    success: boolean;
+    msg: string;
+    error?: string;
+}
+
 
 export default function JoinRoomClient() {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [inputData, setInputData] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+
+
+    async function createRoom(roomName: String) {
+
+        setIsLoading(true);
+
+        try {
+            const response = await fetch(`http://localhost:3000/room/create`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({ slug: roomName })
+            });
+
+            let data: IData | null = null;
+
+            try {
+                data = await response.json();
+            } catch (error) {
+                data = null;
+            }
+
+            if (!response.ok) {
+                toast.error(data?.error || data?.msg || "failed to create room");
+                return;
+            }
+
+            if (data) {
+                if (data.success) {
+                    toast.message(data.msg);
+                    router.refresh();
+                    return;
+                } else {
+                    toast.message(data.msg);
+                    return;
+                }
+            }
+
+        }
+        catch (error) {
+            if (error instanceof TypeError) {
+                toast.error(error.message);
+                return;
+            }
+            if (error instanceof Error) {
+                toast.error(error.message);
+                return;
+            }
+
+            toast.error("failed to create room, something went wrong");
+            return;
+        }
+        finally {
+            setIsLoading(false);
+            setIsOpen(false);
+            setInputData("");
+        }
+    }
+
+
 
     return (
         <>
 
             {
-                isOpen && (<Model setIsOpen={setIsOpen}>
+                isOpen && (<Model clearInputFun={setInputData} setIsOpen={setIsOpen}>
                     <div className="flex justify-between items-center">
                         <h1 className="text-2xl">New Workspace room</h1>
-                        <Button onClick={() => setIsOpen(false)} className="px-2 py-0" variant="danger">X</Button>
+
+                        <Button onClick={() => { setIsOpen(false); setInputData("") }} className="px-2 py-0" variant="danger">X</Button>
                     </div>
 
                     <div className="flex flex-col gap-y-2 mt-8">
                         <label htmlFor="roomName">Room-Name</label>
-                        <Input autoFocus id="roomName" type="text" placeholder="ex: creative-space" />
-                        <Button variant="secondary" className="mt-4 mb-2 text-xl" size="md">Confirm</Button>
+                        <Input name="roomName" value={inputData} onChange={(e) => setInputData(e.target.value)} autoFocus id="roomName" type="text" placeholder="ex: creative-space" />
+
+                        <Button onClick={() => createRoom(inputData)} disabled={inputData.trim() === "" ? true : false} type="button" variant="secondary" className="mt-4 mb-2 text-xl" size="md">
+                            {
+                                isLoading ? <span className="flex items-center justify-center py-0.5"><Loader2 className="animate-spin " /></span> : "Confirm"
+                            }
+                        </Button>
                     </div>
                 </Model>)
             }
--- packages/ui/src/model.tsx
+++ packages/ui/src/model.tsx
@@ -3,17 +3,22 @@ import React from "react";
 
 interface IModel {
     children: React.ReactNode;
-    setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>
+    setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+    clearInputFun?: React.Dispatch<React.SetStateAction<string>>
 }
 
 
 export default function Model(props: IModel) {
 
-    const { children, setIsOpen } = props;
+    const { children, setIsOpen, clearInputFun } = props;
 
     return (
-        <div onClick={() => (setIsOpen && setIsOpen(false))} className="fixed z-50 inset-0 flex items-center justify-center backdrop-brightness-20">
-            <div className="border border-gray-600 max-w-lg w-full py-3 px-5 rounded-md bg-black">
+        <div onClick={() => {
+            setIsOpen && setIsOpen(false);
+            clearInputFun && clearInputFun("");
+        }}
+            className="fixed z-50 inset-0 flex items-center justify-center backdrop-brightness-20">
+            <div onClick={(e) => e.stopPropagation()} className="border border-gray-600 max-w-lg w-full py-3 px-5 rounded-md bg-black">
                 {
                     children
                 }
